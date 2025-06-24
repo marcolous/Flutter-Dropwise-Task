ca780ec00ae1ff92d5995f93b5bd439fddcc0543
marcolous <louismarco226@gmail.com>
2025-06-24 22:08:41 +0300

    first commit

---
+++ lib/models/farm_model.dart
@@ -0,0 +1,17 @@
+class Farm {
+  final int farmId;
+  final String name;
+  final String governorate;
+  final String delegation;
+  final double latitude;
+  final double longitude;
+
+  Farm({
+    required this.farmId,
+    required this.name,
+    required this.governorate,
+    required this.delegation,
+    required this.latitude,
+    required this.longitude,
+  });
+}
